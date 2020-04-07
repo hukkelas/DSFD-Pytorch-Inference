fix: apply softmax on detections output of RetinaNet

face_detection/retinaface/models/retinaface.py
@@ -115,9 +115,8 @@ class RetinaFace(nn.Module):
             [self.ClassHead[i](feature) for i, feature in enumerate(features)],dim=1)
         ldm_regressions = torch.cat(
             [self.LandmarkHead[i](feature) for i, feature in enumerate(features)], dim=1)
-
-        if self.train:
+        if self.training:
             output = (bbox_regressions, classifications, ldm_regressions)
         else:
-            output = (bbox_regressions, classifications.sofmtax(), ldm_regressions)
+            output = (bbox_regressions, classifications.softmax(dim=-1), ldm_regressions)
         return output